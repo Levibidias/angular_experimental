45efa1128817589bf6b67911d2a65ed1e3a55f1b
leviBidias <levibidias03@gmail.com>
2024-03-08 05:33:53 +0100

    testAnimation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { OpencloseComponent } from './openclose/openclose.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path:'',component: OpencloseComponent, title:'page de transition' }
+];
---
+++ src/app/openclose/openclose.component.ts
@@ -0,0 +1,46 @@
+
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+
+@Component({
+  selector: 'app-openclose',
+  standalone: true,
+  imports: [],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.8,
+        backgroundColor: 'blue'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
+  templateUrl: './openclose.component.html',
+  styleUrl: './openclose.component.css',
+})
+export class OpencloseComponent {
+  isOpen = true;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+}
